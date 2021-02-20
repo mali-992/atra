4d2c9b87e5dc61632a49f98caa741052741cb70a
mali-992 <malip3233@gmail.com>
2021-02-20 22:38:57 +0200

    fix

--- models/user.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const userSchema = mongoose.Schema({
-  name:{type: String, required: true},
-  email: { type: String, required: true,unique:true },
-  password:{type:String,required: true},
-  countries:[{type: mongoose.Schema.Types.ObjectId,ref:'Country'}]
-});
-module.exports=mongoose.model('User',userSchema);
--- routes/countryApi.js
+++
@@ -1,9 +0,0 @@
-const router=require('express').Router();
-const {getAll,createCountry,updateCountry,deleteCountry}=require('../controllers/country')
-const checkAuth=require('../middlewares/checkAuth')
-
-router.get('/',checkAuth,getAll);
-router.post('/',checkAuth,createCountry);
-router.put('/:id',checkAuth,updateCountry);
-router.delete('/:id',checkAuth,deleteCountry);
-module.exports=router
